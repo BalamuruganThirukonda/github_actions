f5ecc005d85d573821ddd6df2f2743919185d2f2
BalamuruganThirukonda <mukeshbalamukesh@gmail.com>
2025-01-13 17:59:54 +0100

    correction3 file_test.py

--- file_test.py
+++ file_test.py
@@ -1,26 +1,25 @@
-import pytest
-
+# Removed unnecessary import since pytest is not used directly
+# import pytest
 
 def test_calc_addition():
-  # Function to test the output of 2+4
+    # Function to test the output of 2+4
     output = 2 + 4
     assert output == 6
 
 
 def test_calc_subtraction():
-  # Function to test the output of 2-4
+    # Function to test the output of 2-4
     output = 2 - 4
     assert output == -2
 
 
 def test_calc_multiply():
-  # Function to test the output of 2*4
+    # Function to test the output of 2*4
     output = 2 * 4
     assert output == 8
 
 
 def test_coucou():
-  # Function to test if the output return 'hello'
-    output ='hello'
+    # Function to test if the output return 'hello'
+    output = 'hello'
     assert output == 'hello'
-    
